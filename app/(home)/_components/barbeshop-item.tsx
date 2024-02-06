0edbe31372603342913069b49280c0c693ff515b
Pedro Sobrenome <pedroaugustolopesgomes@example.com>
2024-02-06 00:47:01 -0300

    feat: add initial barbershop item

---
+++ app/(home)/_components/barbeshop-item.tsx
@@ -0,0 +1,53 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Barbershop } from "@prisma/client";
+import { StarIcon } from "lucide-react";
+import Image from "next/image";
+
+interface BarberShopItemProps {
+  barbershop: Barbershop;
+}
+
+const Barbershopitem = ({ barbershop }: BarberShopItemProps) => {
+  return (
+    <Card className="min-w-full max-w-full rounded-2xl">
+      <CardContent className="px-1 py-0">
+        <div className="w-full h-[159px] relative">
+          <div className="absolute top-2 left-2 z-50">
+            <Badge
+              variant="secondary"
+              className="opacity-90 flex gap-1 items-center top-3 left-3"
+            >
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
+            </Badge>
+          </div>
+          <Image
+            alt={barbershop.name}
+            src={barbershop.imageUrl}
+            style={{
+              objectFit: "cover",
+            }}
+            fill
+            className="rounded-2xl"
+          />
+        </div>
+
+        <div className="px-2 pb-3">
+          <h2 className="font-bold mt-2  text-ellipsis text-nowrap">
+            {barbershop.name}
+          </h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.address}
+          </p>
+          <Button className="w-full mt-3" variant="secondary">
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Barbershopitem;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,8 +3,11 @@ import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import Booking from "../_components/booking-item";
+import Barbershopitem from "./_components/barbeshop-item";
+import { db } from "../_lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const barbershop = await db.barbershop.findMany({});
   return (
     <div>
       <Header />
@@ -21,8 +24,21 @@ export default function Home() {
         <Search />
       </div>
       <div className="px-5 mt-6">
-        <h2 className="text-sm uppercase font-bold text-gray-400 mb-3">Agendamentos</h2>
-        <Booking/>
+        <h2 className="text-sm uppercase font-bold text-gray-400 mb-3">
+          Agendamentos
+        </h2>
+        <Booking />
+      </div>
+      
+      <div className="px-5 mt-6">
+        <h2 className="text-sm uppercase font-bold text-gray-400 mb-3">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershop.map((barbershop) => (
+            <Barbershopitem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   );
